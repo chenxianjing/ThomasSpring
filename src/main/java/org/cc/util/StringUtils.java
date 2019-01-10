ed2c17a5488d9a1fe66ef1cbcfcdb278ee35b085
chenxianjing <1531517063@qq.com>
2019-01-10 14:20:47 +0800

    update

--- src/main/java/org/cc/util/StringUtils.java
+++ src/main/java/org/cc/util/StringUtils.java
@@ -1,9 +1,9 @@
 package org.cc.util;
 
 public class StringUtils {
-	
-	public static  boolean isNotEmpty(String str) {
-		if(str == null && str.equals("")) {
+
+	public static boolean isNotEmpty(String str) {
+		if (str == null && str.equals("")) {
 			return false;
 		}
 		return true;
